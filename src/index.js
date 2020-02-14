42f7f835d73e194f23db73b93bab43306f4db894
xujing <754130935@qq.com>
2020-02-14 10:30:28 +0800

    初始化完成

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = React.createElement('h1', null, 'hello react 脚手架!')
+ReactDOM.render(title,document.getElementById('root'))
